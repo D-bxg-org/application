fd65736c0afb0279f8ba05ff5d33b0b79fdd5ebc
D_bxg <ganjinzheng@163.com>
2021-02-10 16:33:04 +0800

    开发主页

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class pages extends Component {
+    render() {
+        return (
+            <div>
+                我是主页
+            </div>
+        )
+    }
+}
