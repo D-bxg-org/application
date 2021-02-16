499e1732ab5422d992375f3c22533863372cbd0d
D_bxg <ganjinzheng@163.com>
2021-02-16 22:34:19 +0800

    完成初步布局

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";//react官方路由库
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom"; // react官方路由库
+
+import { Provider } from "react-redux"; // redux配置
+
+import reportWebVitals from "./reportWebVitals"; // 脚手架自带
+
+import App from "./App.jsx";
+import store from "./redux/store"; // redux的store部分，全局获取
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* 配置总状态 */}
+    <Provider store={store}>
+      {/* 配置总路由 */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
