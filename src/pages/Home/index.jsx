e312990789a26ae5118fd689b70365c566d1d448
D_bxg <ganjinzheng@163.com>
2021-02-16 17:39:17 +0800

    临时开发

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Nav from '../../components/Nav'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="">
+        <Nav></Nav>
         我是主页
         <Link to="/test">
           <button>点我到test页面</button>
