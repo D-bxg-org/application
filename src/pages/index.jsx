e2acc886f3958e6c49580215daf9106e8cc81c06
D_bxg <ganjinzheng@163.com>
2021-02-16 23:42:12 +0800

    完成懒加载

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,25 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Home from "./Home";
-import Test from './Test';
-import Demo from './Demo';
+// loding页面
+import Loding from "../containers/Loding";
+// 懒加载页面
+const Home = lazy(()=>import("./Home"));
+const Test = lazy(()=>import("./Test"));
+const Demo = lazy(()=>import("./Demo"));
 
 export default class pages extends Component {
   render() {
     return (
       <Fragment>
-        <Switch>
-          <Route path="/test" component={Test} />
-          <Route path="/demo" component={Demo} />
-          <Route path="/" component={Home} />
-        </Switch>
+        {/* 部署懒加载中的loding页面 */}
+        <Suspense fallback={<Loding />}>
+          <Switch>
+            <Route path="/test" component={Test} />
+            <Route path="/demo" component={Demo} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Suspense>
       </Fragment>
     );
   }
